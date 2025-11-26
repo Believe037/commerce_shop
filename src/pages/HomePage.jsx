eae798de6f28428f3f22e5d8f26a88065714f248
believe037 <engr.believenduka@gmail.com>
2025-11-26 23:05:48 +0100

    Move checkout page into react

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import './header.css';
 const HomePage = () => {
   return (
     <>
+    <title>eCommerce Shop</title>
       <div className="header">
       <div className="left-section">
         <a href="index.html" className="header-link">
