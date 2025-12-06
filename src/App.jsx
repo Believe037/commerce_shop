3f8a25f8777f14eeb827bb47fcb3032beb311671
“Believe037” <engr.believenduka@gmail.com>
2025-12-06 11:30:32 +0100

    updating semi colon to import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import CheckOut from './pages/CheckOut';
-import OrdersPage from './pages/OrdersPage'
+import OrdersPage from './pages/OrdersPage';
+
 
 
 function App() {
